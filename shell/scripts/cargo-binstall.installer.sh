2969edb25709f9f5408b2c5147bf69e6fd782a78
Blizz <blizz@kernelle-soft.com>
2026-08-27 03:35:16 +0000

    Repoint shellops submodule at GitLab and repin to v0.31.0

--- shell/scripts/cargo-binstall.installer.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-curl -L --proto '=https' --tlsv1.2 -sSf \
-  https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
-  | bash
